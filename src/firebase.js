3673236409b75dba2b8e3248b532d276604b2e8d
Jerry <jerinje@gmail.com>
2026-02-28 00:05:18 +1300

    connect feed to firestore

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo5xW2Munf1bbqYo_wwot9OBjuFTwaPZU",
+  authDomain: "react-learn-1-579a2.firebaseapp.com",
+  projectId: "react-learn-1-579a2",
+  storageBucket: "react-learn-1-579a2.firebasestorage.app",
+  messagingSenderId: "546777000102",
+  appId: "1:546777000102:web:0e7548c3d39fb8df2e2c67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
